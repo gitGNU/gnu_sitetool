a53a31b08a4a311e2cdf7d3f9d55b92049f8846a
Francesco Salvestrini <salvestrini@gmail.com>
2008-02-04 22:36:39 +0100

    Added Home perl module

---
+++ libs/perl/Sitetool/OS/Home.pm
@@ -0,0 +1,69 @@
+#
+# Home.pm
+#
+# Copyright (C) 2007, 2008 Francesco Salvestrini
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+package Sitetool::Base::Home;
+
+use 5.8.0;
+
+use warnings;
+use strict;
+use diagnostics;
+use Config;
+
+use Sitetool::Autoconfig;
+use Sitetool::Base::Debug;
+use Sitetool::OS::Directory;
+
+BEGIN {
+    use Exporter ();
+    our ($VERSION, @ISA, @EXPORT);
+    
+    @ISA    = qw(Exporter);
+    @EXPORT = qw(&home);
+}
+
+sub home ()
+{
+    my $osname;
+    my $homedir;
+
+    $osname  = $Config{osname};
+    $homedir = undef;
+    if ($osname =~ /MSWin32/) {
+	bug("MSWin32 not yet supported")
+    } elsif ($osname =~ /darwin/) {
+	bug("darwin not yet supported")
+    } elsif ($osname =~ /MacOS9/) {
+	bug("MacOS9 not yet supported")
+    } else {
+	$homedir = $ENV{HOME};
+	bug("Cannot detect home directory, architecture unsupported")
+    }
+
+    assert(defined($homedir));
+
+    if (!directory_ispresent($homedir)) {
+	bug("Problems detecting home directory");
+    }
+
+    return $homedir;
+}
+
+1;
