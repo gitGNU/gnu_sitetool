f3040f8f59a160f087c5e389889db672f62f2b1e
Francesco Salvestrini <salvestrini@gmail.com>
2008-02-15 22:30:15 +0100

    Update site pages

--- site/index.scm
+++ site/index.scm
@@ -1,44 +1,43 @@
 (div (@ (id "main_body"))
-     (h3 "DESCRIPTION")
      (p "Sitetool is a CMS that should work with (or without) the autotools."
-	(br)
+	(br) (br)
 	"It aims to aid developers building their projects web-sites
      without having to write HTML directly (or via strange machineries)
      while having a \"coding\" approach to the generation of their contents."
 	(br) (br)
 	"Sitetool is divided in (mainly) two layers: backend and frontends."
-	(br) (br)
 	"The backend is the lower layer. It decouples the different frontends
-     using a common language: SXML. "
+     using a common format: "
 	(a (@ (href "http://okmij.org/ftp/Scheme/xml.html")) SXML)
-	" is Scheme-XML."
-	(br)
-	"In other words we use Scheme to build XML/HTML pages. Using this
-     approach pages are \"code\", not only mindless ASCII files. The
-     user will be able to \"code\" its pages..."
-	(br)
-	"The frontends form the upper layer (the layer that the common user
-     usually see). Each frontends translate contents to the common
-     backend language (SXML)."
+	". "
+	"Frontends form the upper layer (the layer that the common user
+     usually should see). Each frontend translates a content to a different
+     format. Frontends chains are automatically formed (or manually specified)
+     in order to transform the content from its input format to the backend
+     format"
 	(br) (br)
-	"Each frontend understands different input: wikitext-like syntax,
-     structured-text, m4, SXML-pass-through, ChangeLog, NEWS ..."
-	(br)
 	"The upper layer is fully expandable with python, perl, shell
      scripts and so on."
-	(br) (br)
-	"Using this approach users (package developers) are not required to
-     write their sites using scheme directly. Their contents get
-     translated from the choosen frontend to the backend via
-     filters-chain: the user writes its contents by choosing one (or
-     more) of the filters available then the content get translated to
-     SXML, from SXML to scheme and finally to HTML.")
+	)
+
      (h3 "AUTHORS")
-     (p
-      "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-      (br)
-      "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
+     (p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+	(br)
+	"Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
+	)
+
      (h3 "RELEASE")
      (p
-      "No public release available yet...")
+      "Sorry, no public release available at the moment."
+      )
+     
+     (h3 "DEVELOPMENT")
+     (p
+      "You can Browse the "
+      (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
+	 "Git repository")
+      " of this project with your web
+     browser. This gives you a good picture of the current status of the
+     source files. You may also view the complete histories of any file in the
+     repository as well as differences among two versions.")
      )
