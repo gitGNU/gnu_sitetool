c45e4b7dab12c7d19ce69b0866d72ce605487d96
Alessandro Massignan <ff0000.it@gmail.com>
2008-02-18 09:20:43 +0100

    Updated and fixed site layout

--- site/index.scm
+++ site/index.scm
@@ -1,43 +1,38 @@
-(div (@ (id "main_body"))
-     (p "Sitetool is a CMS that should work with (or without) the autotools."
-	(br) (br)
-	"It aims to aid developers building their projects web-sites
-     without having to write HTML directly (or via strange machineries)
-     while having a \"coding\" approach to the generation of their contents."
-	(br) (br)
-	"Sitetool is divided in (mainly) two layers: backend and frontends."
-	"The backend is the lower layer. It decouples the different frontends
-     using a common format: "
-	(a (@ (href "http://okmij.org/ftp/Scheme/xml.html")) SXML)
-	". "
-	"Frontends form the upper layer (the layer that the common user
-     usually should see). Each frontend translates a content to a different
-     format. Frontends chains are automatically formed (or manually specified)
-     in order to transform the content from its input format to the backend
-     format"
-	(br) (br)
-	"The upper layer is fully expandable with python, perl, shell
-     scripts and so on."
-	)
-
-     (h3 "AUTHORS")
-     (p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-	(br)
-	"Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
-	)
-
-     (h3 "RELEASE")
-     (p
-      "Sorry, no public release available at the moment."
-      )
-     
-     (h3 "DEVELOPMENT")
-     (p
-      "You can Browse the "
-      (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
-	 "Git repository")
-      " of this project with your web
-     browser. This gives you a good picture of the current status of the
-     source files. You may also view the complete histories of any file in the
-     repository as well as differences among two versions.")
-     )
+(h3 "DESCRIPTION")
+(p "Sitetool is a CMS that should work with (or without) the autotools."
+   (br)
+   "It aims to aid developers building their projects web-sites
+    without having to write HTML directly (or via strange machineries)
+    while having a \"coding\" approach to the generation of their contents."
+   (br)
+   "Sitetool is divided in (mainly) two layers: backend and frontends.
+    The backend is the lower layer. It decouples the different frontends
+    using a common format: "
+    (a (@ (href "http://okmij.org/ftp/Scheme/xml.html")) SXML)
+   ". "
+   (br)
+   "Frontends form the upper layer (the layer that the common user
+    usually should see). Each frontend translates a content to a different
+    format. Frontends chains are automatically formed (or manually specified)
+   in order to transform the content from its input format to the backend
+    format."
+   (br)
+   "The upper layer is fully expandable with python, perl, shell scripts
+    and so on."
+ )
+(h3 "AUTHORS")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+   (br)
+   "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
+ )
+(h3 "RELEASE")
+(p "Sorry, no public release available at the moment.")
+(h3 "DEVELOPMENT")
+(p "You can Browse the "
+   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
+      "Git repository")
+   " of this project with your web browser. This gives you a good
+    picture of the current status of the source files. You may also view
+    the complete histories of any file in the repository as well as
+    differences among two versions."
+ )
