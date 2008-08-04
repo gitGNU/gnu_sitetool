2bb5bfa02e36c5b0c605d193333926f6c81f7a10
Alessandro Massignan <ff0000.it@gmail.com>
2008-08-04 23:44:53 +0200

    Update whole site structure

--- site/in/index.scm
+++ site/in/index.scm
@@ -4,30 +4,34 @@
  (a (@ (href "http://en.wikipedia.org/wiki/Content_management_system")) CMS)
  " that should work with (or without) the "
  (a (@ (href "http://en.wikipedia.org/wiki/Autotools")) autotools)
- ".")
-(br)
-(p "The project should aid developers building their projects web-sites "
-   "without having to write HTML directly (or via strange machineries) "
-   "while having a \"coding\" approach to the generation of their contents.")
-(br)
-(p "Sitetool content production is divided in two layers: backend and "
-   "frontends.")
-(br)
-(p "The backend is the lower layer, it decouples the different frontends "
-   "using " (a (@ (href "http://okmij.org/ftp/Scheme/xml.html")) SXML) " "
-   "as common format.")
-(br)
-(p "Frontends form the upper layer, each frontend translates a content from "
-   "its format to the backend format using a chain of filters. "
-   "Filters chains are automatically formed (or manually specified) in order "
-   "to transform each content from its input format to the backend "
-   "format. ")
-(br)
+ "."
+ (br)
+ (br)
+ "The project should aid developers building their projects web-sites "
+ "without having to write HTML directly (or via strange machineries) "
+ "while having a \"coding\" approach to the generation of their contents."
+ (br)
+ (br)
+ "Sitetool content production is divided in two layers: backend and "
+ "frontends."
+ (br)
+ (br)
+ "The backend is the lower layer, it decouples the different frontends "
+ "using " (a (@ (href "http://okmij.org/ftp/Scheme/xml.html")) SXML) " "
+ "as common format."
+ (br)
+ (br)
+ "Frontends form the upper layer, each frontend translates a content from "
+ "its format to the backend format using a chain of filters. "
+ "Filters chains are automatically formed (or manually specified) in order "
+ "to transform each content from its input format to the backend "
+ "format. "
+ (br))
 (h4 "Filters")
-(p "A filter is nothing more than a program which takes an input file and "
-   "traslates it to an output file, usually in a different format. "
-   "Sitetool comes with a set of filters:")
-
+(p
+ "A filter is nothing more than a program which takes an input file and "
+ "traslates it to an output file, usually in a different format. "
+ "Sitetool comes with a set of filters:")
 (ul
  (li "changelog-wikitext")
  (li "map-sxml")
@@ -36,7 +40,6 @@
  (li "sxml-sxml")
  (li "wikitext-sxml")
  )
-(br)
 (p "Examples:")
 (ul
  (li "The filters chain used to transform a ChangeLog file to a backend "
@@ -46,226 +49,243 @@
  (li "A wikitext content get translated to SXML using the "
      "wikitext-sxml -> sxml-sxml chain")
  )
-(br)
-(p "In order to use third-party filters or your own filters, the "
-   (tt "sitetool-config") " script should be used to find the filters "
-   "installation directory:")
-(pre
- "Usage: sitetool-config [OPTIONS]" (br)
- "" (br)
- "OPTIONS:" (br)
- "    [-h|--help]" (br)
- "    [--version]" (br)
- "    [--filters-check]" (br)
- "    [--filters-dir]" (br)
- "    [--styles-dir]" (br)
- )
+(p
+ "In order to use third-party filters or your own filters, the "
+ (samp "sitetool-config") " script should be used to find the filters "
+ "installation directory:")
+(pre "
+     Usage: sitetool-config [OPTIONS]
+
+     OPTIONS:
+	[-h|--help]
+	[--version]
+	[--filters-dir]
+	[--styles-dir]
+")
+(p
+ "Simply place additional filters into the filters directory: "
+ (kbd "cp my-filter `sitetool-config --filters-dir`"))
 
 (h4 "Use")
-(p "Sitetool is composed by a bunch of utilities. Each utility could be "
-   "invoked manually or by the main program " (tt "sitetool") " via the "
-   (tt "--mode") " option. The main program invocation has the advantage of "
-   "handling the inter-utility dependencies automatically. A common set of "
-   "parameters are available to each utility.")
-(pre
- "Usage: sitetool [OPTION]... [-- [MODE-ARG]...]" (br)
- "" (br)
- "Options:" (br)
- "  -M, --mode=MODE            Running mode is MODE" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(p
+ "Sitetool is composed by a bunch of utilities. Each utility could be "
+ "invoked manually or by the main program " (samp "sitetool") " via the "
+ (samp "--mode") " option. The main program invocation has the advantage of "
+ "handling the inter-utility dependencies automatically. A common set of "
+ "parameters are available to each utility.")
+(pre "
+     Usage: sitetool [OPTION]... [-- [MODE-ARG]...]
 
-(p "Current available modes are:")
+     Options:
+	-M, --mode=MODE            Running mode is MODE
+
+     [COMMON-PARAMETERS]
+")
+
+(p
+ "Current available modes are:"
+ (br))
+
 (h5 "auths")
-(pre
- "Usage: sitetool-auths [OPTIONS]" (br)
- "" (br)
- "  -i, --initialize            initialize the authorizations DB" (br)
- "  -A, --add=HOST,LOGIN,PSWD   add a DB entry" (br)
- "  -D, --delete=HOST,LOGIN     remove a DB entry" (br)
- "  -S, --show                  show all DB entries" (br)
- "  -C, --clear                 clear all DB entries" (br)
- "" (br)
- "    HOST  is host=HOSTNAME" (br)
- "    LOGIN is login=LOGINNAME" (br)
- "    PSWD  is password=PASSWORD" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-auths [OPTIONS]
+
+	    -i, --initialize            initialize the authorizations DB
+	    -A, --add=HOST,LOGIN,PSWD   add a DB entry
+	    -D, --delete=HOST,LOGIN     remove a DB entry
+	    -S, --show                  show all DB entries
+	    -C, --clear                 clear all DB entries
+
+	    HOST  is host=HOSTNAME
+	    LOGIN is login=LOGINNAME
+	    PSWD  is password=PASSWORD
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "install")
-(pre
- "Usage: sitetool-install [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -i, --input-dir=DIR         source directory is DIR" (br)
- "  -H, --hname=HOST            destination host is HOST" (br)
- "  -P, --hpassword=PASSWD      password for host HOST is PASSWD" (br)
- "  -U, --huser=USER            user for host HOST is USER" (br)
- "  -o, --hdir=DIR              destination directory is DIR" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-install [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -i, --input-dir=DIR         source directory is DIR
+	    -H, --hname=HOST            destination host is HOST
+	    -P, --hpassword=PASSWD      password for host HOST is PASSWD
+	    -U, --huser=USER            user for host HOST is USER
+	    -o, --hdir=DIR              destination directory is DIR
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "parse")
-(pre
- "Usage: sitetool-parse [OPTIONS]" (br)
- "" (br)
- "  -i, --input=FILE            use FILE as input file" (br)
- "  -o, --output=FILE           use FILE as output file" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-parse [OPTIONS]
+
+	    -i, --input=FILE            use FILE as input file
+	    -o, --output=FILE           use FILE as output file
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "initialize")
-(pre
- "Usage: sitetool-initialize [OPTIONS]" (br)
- "" (br)
- "  -w, --work-dir=DIR          working directory is DIR" (br)
- "  -o, --output-dir=DIR        output directory is DIR" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-initialize [OPTIONS]
+
+	    -w, --work-dir=DIR          working directory is DIR
+	    -o, --output-dir=DIR        output directory is DIR
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "uninstall")
-(pre
- "Usage: sitetool-uninstall [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -i, --input-dir=DIR         source directory is DIR" (br)
- "  -H, --hname=HOST            destination host is HOST" (br)
- "  -P, --hpassword=PASSWD      password for host HOST is PASSWD" (br)
- "  -U, --huser=USER            user for host HOST is USER" (br)
- "  -o, --hdir=DIR              destination directory is DIR" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-uninstall [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -i, --input-dir=DIR         source directory is DIR
+	    -H, --hname=HOST            destination host is HOST
+	    -P, --hpassword=PASSWD      password for host HOST is PASSWD
+	    -U, --huser=USER            user for host HOST is USER
+	    -o, --hdir=DIR              destination directory is DIR
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "clean")
-(pre
- "Usage: sitetool-clean [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -w, --work-dir=DIR          use DIR as a working directory" (br)
- "  -o, --output-dir=DIR        place output into directory DIR" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-clean [OPTIONS]
+
+	  -c, --configuration=FILE    configuration file is FILE
+	  -w, --work-dir=DIR          use DIR as a working directory
+	  -o, --output-dir=DIR        place output into directory DIR
+
+     [COMMON-PARAMETERS]
+")
 
 (h5 "build")
-(pre
- "Usage: sitetool-build [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -w, --work-dir=DIR          use DIR as a working directory" (br)
- "  -o, --output-dir=DIR        place output into directory DIR" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-build [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -w, --work-dir=DIR          use DIR as a working directory
+	    -o, --output-dir=DIR        place output into directory DIR
+
+    [COMMON-PARAMETERS]
+")
 
 (h5 "check")
-(pre
- "Usage: sitetool-check [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -o, --output-dir=DIR        use DIR as output directory" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-check [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -o, --output-dir=DIR        use DIR as output directory
+
+    [COMMON-PARAMETERS]
+")
 
 (h5 "digest")
-(pre
- "Usage: sitetool-digest [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -o, --output-dir=DIR        use DIR as output directory" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-digest [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -o, --output-dir=DIR        use DIR as output directory
+
+    [COMMON-PARAMETERS]
+")
 
 (h5 "preprocess")
-(pre
- "Usage: sitetool-preprocess [OPTIONS]" (br)
- "" (br)
- "  -D, --define=VAR=VALUE      define variable VAR to value VALUE" (br)
- "  -U, --undefine=VAR          undefine variable VAR" (br)
- "  -i, --input=FILE            use FILE as input file" (br)
- "  -o, --output=FILE           use FILE as output file" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-preprocess [OPTIONS]
+
+	    -D, --define=VAR=VALUE      define variable VAR to value VALUE
+	    -U, --undefine=VAR          undefine variable VAR
+	    -i, --input=FILE            use FILE as input file
+	    -o, --output=FILE           use FILE as output file
+
+    [COMMON-PARAMETERS]
+")
 
 (h5 "validate")
-(pre
- "Usage: sitetool-validate [OPTIONS]" (br)
- "" (br)
- "  -c, --configuration=FILE    configuration file is FILE" (br)
- "  -o, --output-dir=DIR        use DIR as output directory" (br)
- "" (br)
- "[COMMON-PARAMETERS]" (br)
- )
+(pre "
+     Usage: sitetool-validate [OPTIONS]
+
+	    -c, --configuration=FILE    configuration file is FILE
+	    -o, --output-dir=DIR        use DIR as output directory
+
+    [COMMON-PARAMETERS]
+
+")
 
 (h3 "COPYING")
-(p "Sitetool is licensed under the "
-   (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
-      "GNU General Public License, version 2"))
+(p
+ "Sitetool is licensed under the "
+ (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
+ "GNU General Public License, version 2"))
 
 (h3 "MAINTAINERS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
+(p
+ "Francesco Salvestrini <salvestrini AT gmail DOT com>")
 
 (h3 "AUTHORS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-   (br)
-   "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
-   )
+(p
+ "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+ (br)
+ "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
 
 (h3 "RELEASES")
-(p "Sorry, no public release available at the moment.")
+(p
+ "Sorry, no public release available at the moment.")
 
 (h3 "MAILING LISTS")
-(p "Sitetool has a single moderated mailing list, with an archive. "
-   "In order to post a message to the mailing list you must be subscribed. "
-   "Please consult the "
-   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic") )
-      "Sitetool mailing list page")
-   " for more information on subscribing to the mailing list.")
+(p
+ "Sitetool has a single moderated mailing list, with an archive. "
+ "In order to post a message to the mailing list you must be subscribed. "
+ "Please consult the "
+ (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic") )
+  "Sitetool mailing list page")
+ " for more information on subscribing to the mailing list.")
 
 (h3 "REPORT A BUG")
-(p "If you think you have found a bug in Sitetool then please send as "
-   "complete a report as possible to <sitetool AT nongnu.org>. An easy way "
-   "to collect all the required information, such as platform and compiler, "
-   "is to include in your report the config.log file available at the end of "
-   "the configuration procedure. ")
-(br)
-(p "If you have a patch for a bug in Sitetool that hasn't yet been fixed in "
-   "the latest repository sources, please be so kind to create it using the "
-   "repository sources, not the release sources.")
+(p
+ "If you think you have found a bug in Sitetool then please send as "
+ "complete a report as possible to <sitetool AT nongnu.org>. An easy way "
+ "to collect all the required information, such as platform and compiler, "
+ "is to include in your report the config.log file available at the end of "
+ "the configuration procedure. "
+ (br)
+ (br)
+ "If you have a patch for a bug in Sitetool that hasn't yet been fixed in "
+ "the latest repository sources, please be so kind to create it using the "
+ "repository sources, not the release sources.")
 
 (h3 "DEVELOPMENT")
 (h4 "Browsing sources")
-(p "You can browse the "
-   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
-      "Git repository")
-   " of this project with your web browser. This gives you a good "
-   "picture of the current status of the source files. You may also view "
-   "the complete histories of any file in the repository as well as "
-   "differences among two versions.")
+(p
+ "You can browse the "
+ (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
+  "Git repository")
+ " of this project with your web browser. This gives you a good "
+ "picture of the current status of the source files. You may also view "
+ "the complete histories of any file in the repository as well as "
+ "differences among two versions.")
 
 (h4 "Getting a copy of the Git Repository")
-(p "Anonymous checkout:")
-(br)
-(tt (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
-    " clone git://git.savannah.nongnu.org/sitetool.git")
+(p
+ "Anonymous checkout:"
+ (br)
+ (samp (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
+	" clone git://git.savannah.nongnu.org/sitetool.git"))
 
 (h4 "Contribute")
-(p "If you have time and programming skills, you can help us by developing "
-   "missing features, regression tests or bug-fixing the present codebase. "
-   "Subscribe to the "
-   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic") )
-      "mailing list")
-   ", drop us a mail and start coding. Send your code to the "
-   "mailing list under the form of patches for the current revision system.")
-(br)
-(p "If you have time but no programming skills, you can help with "
-   "documentation,  packaging, tests before releases etc ...")
+(p
+ "If you have time and programming skills, you can help us by developing "
+ "missing features, regression tests or bug-fixing the present codebase. "
+ "Subscribe to the "
+ (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic"))
+  "mailing list")
+ ", drop us a mail and start coding. Send your code to the "
+ "mailing list under the form of patches for the current revision system."
+ (br)
+ (br)
+ "If you have time but no programming skills, you can help with "
+ "documentation,  packaging, tests before releases etc ...")
