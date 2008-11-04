fa4f95d4a7be1b3975e7ab5db8a31e5069743ade
Francesco Salvestrini <salvestrini@gmail.com>
2008-11-04 23:08:41 +0100

    Updated site

--- site/in/index.scm
+++ site/in/index.scm
@@ -4,10 +4,8 @@
    " that should aid developers building their projects web-sites "
    "without having to write HTML directly (or via strange machineries) "
    "while having a \"programming\" approach to the generation of their "
-   "contents."
-   (br)
-   (br)
-   "Sitetool site/contents generation is divided in two layers: "
+   "contents.")
+(p "Sitetool site/contents generation is divided in two layers: "
    (i "backend") " and " (i "frontends") ".")
 (ul
  (li "The backend is the lower layer, it decouples the different frontends "
@@ -93,6 +91,7 @@
 
 ")
 (p "To use external filters ... simply copy them  into the filters directory.")
+
 (h4 "Use")
 (p "Sitetool is composed by a bunch of utilities. Each utility could be "
    "invoked manually or by using the main program " (samp "sitetool") " via "
@@ -100,8 +99,7 @@
    "advantage of handling the inter-utility dependencies automatically.")
 (p "The complete input " (a (@ (href "./grammar.html")) "grammar") " "
    "is available.")
-(p "Current available modes are:"
-   (br))
+(p "Current available modes are:")
 (ul
  (li "parse")
  (li "initialize")
@@ -112,20 +110,22 @@
  (li "preprocess")
  (li "validate")
  )
+
 (h3 "COPYING")
 (p "The project is licensed under the "
    (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
       "GNU General Public License, version 2"))
+
 (h3 "MAINTAINERS")
 (p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
 
 (h3 "AUTHORS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-   (br)
-   "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
+(p "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
 
 (h3 "RELEASES")
 (p "Sorry, no public release available at the moment.")
+
 (h3 "MAILING LISTS")
 (p "The project has a single moderated mailing list, with an archive. "
    "In order to post a message to the mailing list you must be subscribed. "
@@ -133,19 +133,20 @@
    (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic") )
       "mailing list page")
    " for more information on subscribing to the mailing list.")
+
 (h3 "REPORT A BUG")
 (p "If you think you have found a bug then please send as complete a report "
    "as possible to "
    "<sitetool-generic AT nongnu DOT org>. "
    "An easy way to collect all the required information, such as platform and "
    "compiler, is to include in your report the config.log file available at "
-   "the end of the configuration procedure. "
-   (br)
-   (br)
-   "If you have a patch for a bug that hasn't yet been fixed in "
+   "the end of the configuration procedure.")
+(p "If you have a patch for a bug that hasn't yet been fixed in "
    "the latest repository sources, please be so kind to create it using the "
    "repository sources, not the release sources.")
+
 (h3 "DEVELOPMENT")
+
 (h4 "Browsing sources")
 (p "You can browse the "
    (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=sitetool.git"))
@@ -154,11 +155,13 @@
    "picture of the current status of the source files. You may also view "
    "the complete histories of any file in the repository as well as "
    "differences among two versions.")
+
 (h4 "Getting a copy of the Git Repository")
-(p "Anonymous checkout: ")
+(p "Anonymous checkout:")
 (pre (@ class "terminal")
      (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) "git")
      " clone git://git.savannah.nongnu.org/sitetool.git")
+
 (h4 "Contribute")
 (p "If you have time and programming skills, you can help us by developing "
    "missing features, regression tests or bug-fixing the present codebase. "
@@ -166,8 +169,6 @@
    (a (@ (href "http://lists.nongnu.org/mailman/listinfo/sitetool-generic"))
       "mailing list")
    ", drop us a mail and start coding. Send your code to the "
-   "mailing list under the form of patches for the current revision system."
-   (br)
-   (br)
-   "If you have time but no programming skills, you can help with "
+   "mailing list under the form of patches for the current revision system.")
+(p "If you have time but no programming skills, you can help with "
    "documentation,  packaging, tests before releases etc ...")
